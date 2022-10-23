172c95caea2c1a3422ac7d7a62df16549dc85a50
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-23 19:49:06 +0500

    add logic

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        String english = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        if (english.indexOf(symbol) != -1){
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
