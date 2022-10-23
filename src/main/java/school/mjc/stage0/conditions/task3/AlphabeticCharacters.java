b842680523e430846ce83aa28aec56fe727d8254
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-23 19:47:44 +0500

    add logic

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String english = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String vowels = "aeiou";
+        if (english.indexOf(character) != -1){
+            if (vowels.indexOf(character) != -1){
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
