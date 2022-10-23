6dcaa6cf60b44cd9b566fa2afbf2c89a13033623
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-23 19:48:21 +0500

    add logic

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month > 0 && month < 13){
+            if (month == 2){
+                System.out.println(28);
+            } else if (month % 2 == 0){
+                System.out.println(30);
+            } else {
+                System.out.println(31);
+            } 
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
